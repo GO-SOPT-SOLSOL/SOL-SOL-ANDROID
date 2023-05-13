78b647d33585a808b4defc61380aaed2d8342e50
KwakEuiJin <yuijin0909@gmail.com>
2023-05-14 01:45:46 +0900

    First Commit

---
+++ app/src/main/java/com/solosol/solsolandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.solosol.solsolandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
