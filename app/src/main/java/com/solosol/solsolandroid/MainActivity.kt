49e8b39e58d9a81b636ebb422d9f217058548a3b
KwakEuiJin <yuijin0909@gmail.com>
2023-05-18 23:41:41 +0900

    feat(home): Home 화면 기초셋팅

--- app/src/main/java/com/solosol/solsolandroid/MainActivity.kt
+++ app/src/main/java/com/solosol/solsolandroid/MainActivity.kt
@@ -2,10 +2,20 @@ package com.solosol.solsolandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.solosol.solsolandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            true
+        }
     }
 }
