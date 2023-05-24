acbd8536b51de95dc31e4a81f79145b211fbf4b6
곽의진 <yuijin0909@gmail.com>
2023-05-24 23:41:29 +0900

    feat(transfer3): api 로직 변경

--- app/src/main/java/com/solosol/solsolandroid/transfer3/Transfer3Activity.kt
+++ app/src/main/java/com/solosol/solsolandroid/transfer3/Transfer3Activity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
 import com.solosol.solsolandroid.databinding.ActivityTransfer3Binding
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -29,42 +31,32 @@ class Transfer3Activity : AppCompatActivity() {
     }
 
     private fun completeTransfer() {
-        transferService.postTransfer(// 서버로 보낼 데이터 생성
-            with(binding) {
-                RequestTransferDto(
-                    1,
-                    tvPriceTransfer3.text.toString().replace(",","").toInt(),
-                    tvBankTransfer3.text.toString(),
-                    tvNumberTransfer3.text.toString(),
-                    edtTransferMemoTransfer3.text.toString(),
-                    edtReceiverMemoTransfer3.text.toString(),
-                    0
-                )
-            }).enqueue(object :
-            Callback<ResponseTransferDto> {
-            override fun onResponse(
-                call: Call<ResponseTransferDto>,
-                response: Response<ResponseTransferDto>
-            ) {
-                if (response.isSuccessful) {
-                    val responseTransferDto = response.body()
-                    if (responseTransferDto != null) {
-                        if (responseTransferDto.success) {
-                            Log.d("Transfer 서버연결", "onResponse: ${responseTransferDto.message}")
-                            Toast.makeText(this@Transfer3Activity, "성공", Toast.LENGTH_SHORT).show()
-                        } else {
-                            Log.d("Transfer 서버연결실패", "onResponse: ${responseTransferDto.message}")
-                            Snackbar.make(binding.root, "실패", Snackbar.LENGTH_SHORT).show()
-                        }
-                    }
-                }
+        val requestTransferDto: RequestTransferDto = requestTransferDto()
+
+        lifecycleScope.launch {
+            val response = transferService.postTransfer(requestTransferDto)
+            if (response.isSuccessful) {
+                Log.d("Transfer 서버연결", "onResponse: ${response.message()}")
+                Toast.makeText(this@Transfer3Activity, "성공", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("Transfer 서버연결실패", "onResponse: ${response.message()}")
+                Snackbar.make(binding.root, "실패", Snackbar.LENGTH_SHORT).show()
             }
-
-            override fun onFailure(call: Call<ResponseTransferDto>, t: Throwable) {
-                Log.e("Transfer 서버연결", "onFailure: $t")
-            }
-        })
+        }
+    }
 
 
+    private fun requestTransferDto(): RequestTransferDto {
+        return with(binding) {
+            RequestTransferDto(
+                1,
+                tvPriceTransfer3.text.toString().replace(",", "").toInt(),
+                tvBankTransfer3.text.toString(),
+                tvNumberTransfer3.text.toString(),
+                edtTransferMemoTransfer3.text.toString(),
+                edtReceiverMemoTransfer3.text.toString(),
+                0
+            )
+        }
     }
 }
--- app/src/main/java/com/solosol/solsolandroid/transfer3/TransferService.kt
+++ app/src/main/java/com/solosol/solsolandroid/transfer3/TransferService.kt
@@ -1,12 +1,13 @@
 package com.solosol.solsolandroid.transfer3
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface TransferService {
-    @POST("api/transfers?memberId=1")
-    fun postTransfer(
+    @POST("/api/transfers?memberId=1")
+    suspend fun postTransfer(
         @Body request: RequestTransferDto,
-    ): Call<ResponseTransferDto>
+    ): Response<ResponseTransferDto>
 }
